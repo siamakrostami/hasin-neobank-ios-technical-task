c2ff7c6afcc7f8ea4929b4a92d317a8e87b36105
Siamak Rostami <srostami2005@gmail.com>
2022-07-11 17:19:38 +0430

    Feat: - working on search

---
+++ Spotifier/Controllers/Search/SearchViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SearchViewController.swift
+//  Spotifier
+//
+//  Created by Siamak Rostami on 7/11/22.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
